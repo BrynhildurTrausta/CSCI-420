2f97206903b313a93f31b07f237d00cb81cffa85
Brynhildur Traustadottir <binnatr@gmail.com>
2023-10-02 20:44:31 -0400

    users name displays for everyone. Comments added.

--- HW3-UDP_Socket/client.py
+++ HW3-UDP_Socket/client.py
@@ -2,6 +2,8 @@
 	Name: Brynhildur Traustadottir
 	Date: 09/27/23
 	Desc: Terminal UDP Chat System - The client
+		  Include the time that each message was sent to all users
+		  Allow the user to set their name to an address
 '''
 import socket
 import threading
@@ -13,6 +15,8 @@ ip_server = "127.0.0.1" # Me. The machine you plan on communicating with
 sock_ready = False
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# The function that receives the messages from everyone
+# that is sent to the server
 def listenThread():
 	global s, sock_ready
 	while not sock_ready:
@@ -22,16 +26,20 @@ def listenThread():
 		msg = msg.decode('UTF-8')
 		print(msg)
 
+# Asks the client for its name
 def setName():
 	global sock_ready
 	name = input("Please enter your name: ")
 	sock_ready = True
 	return name
 
+# The thread to allow messages to be displayed to clients 
+# sent by others
 thread = threading.Thread(target=listenThread).start()
 
 username = setName()
 
+# Sends the name to the server
 s.sendto(f"/Name {username}".encode('UTF-8'), (ip_server, port) ) 
 
 while True:
--- HW3-UDP_Socket/server.py
+++ HW3-UDP_Socket/server.py
@@ -2,6 +2,8 @@
 	Name: Brynhildur Traustadottir
 	Date: 09/27/23
 	Desc: Terminal UDP Chat System - The Server
+		  Include the time that each message was sent to all users
+		  Allow the user to set their name to an address
 '''
 
 import socket
@@ -15,26 +17,34 @@ s.bind( ('0.0.0.0', listen_port) )
 clients = {}
 
 while True:
+	# Getting the message from the client
 	print("Waiting for message...")
 	(msg, addr) = s.recvfrom(1024)
 	msg = msg.decode('UTF-8')
 
+	# If the message starts with /Name, the client had submitted
+	# His name as the first message
+	# That name will be the value to the key in the dictionary
+	# Which is the Ip address and the port number
 	if msg.startswith('/Name '):
-		Name = msg.split(" ", 1)[1]
-		clients[addr] = clients.get(addr, f'{Name}')
+		name = msg.split(" ", 1)[1]
+		clients[addr] = name
 		print(clients)
 		continue
 
-	clients[addr] = clients.get(addr, f'{Name}')
+	# Getting the correct name that matches the ip address
+	sender_name = clients.get(addr, 'Unknown')
 	print(clients)
 
+	# Getting the real time in military time
 	now = datetime.now()
 	currentTime = now.strftime("%H:%M:%S")
 
-	print(f"At {currentTime} {addr} said: {msg}")
+	print(f"At {currentTime} {addr} ({sender_name}) said: {msg}")
 
 	msg = f'{addr}: {msg.lower()}'
 
+	# sending the messages and the name of the sender to all clients
 	for c in clients:
-		s.sendto(msg.encode('UTF-8'), c )
+		s.sendto(f"From {sender_name}: {msg}".encode('UTF-8'), c )
 
